6ac801e953b5345dd3099931fa88e1125d8fc1e6
Raphaël Pinson <raphael.pinson@camptocamp.com>
2012-04-14 23:13:04 +0200

    Make 'match' and 'print' commands for the augeas application, in a similar way to augtool

--- augeasquery/augeas.application.rb
+++ augeasquery/augeas.application.rb
@@ -1,8 +1,9 @@
 class MCollective::Application::Augeas<MCollective::Application
   description "Client to the AugeasQuery Plugin Execution system"
-  usage "Usage: augeas <query>"
+  usage "Usage: augeas [match|print] <path>"
 
   def post_option_parser(configuration)
+    configuration[:command] = ARGV.shift if ARGV.size > 0
     configuration[:query] = ARGV.shift if ARGV.size > 0
   end
 
@@ -11,20 +12,41 @@ class MCollective::Application::Augeas<MCollective::Application
   end
 
   def main
-    augeas = rpcclient("augeasquery")
 
-    augeas_results = augeas.query(:query => configuration[:query])
+    case configuration[:command]
+    when "match"
+      augeas = rpcclient("augeasquery")
 
-    puts
+      augeas_results = augeas.query(:query => configuration[:query])
 
-    augeas_results.each do |result|
-      printf("%-40s\n", result[:sender])
-      result[:data][:matched].each do |matched|
-        printf("    %-40s\n", matched)
+      puts
+
+      augeas_results.each do |result|
+        printf("%-40s\n", result[:sender])
+        result[:data][:matched].each do |matched|
+          printf("    %-40s\n", matched)
+        end
       end
-    end
 
-    puts
+      puts
+
+    when "print"
+      augeas = rpcclient("augeasquery")
+
+      augeas_results = augeas.query(:query => configuration[:query]+"|"+configuration[:query]+"//*")
+
+      puts
+
+      augeas_results.each do |result|
+        printf("%-40s\n", result[:sender])
+        result[:data][:matched].each do |matched|
+          printf("    %-40s\n", matched)
+        end
+      end
+
+      puts
+
+    end
 
   end
 end
